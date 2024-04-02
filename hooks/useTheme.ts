fc0700074772f79042aae85f98d74e1682b3c4e9
Jatin Kumar <jk8826984588@gmail.com>
2024-04-02 23:49:36 +0530

    implemented dark mode

---
+++ hooks/useTheme.ts
@@ -0,0 +1,53 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import useStorage from "./useStorage";
+import { MEDIA, NAMESPACE, THEME } from "@/constants";
+
+export default function useTheme() {
+  const { setItem, getItem } = useStorage(NAMESPACE);
+  const [theme, setTheme] = useState<string>(() => {
+    const prevTheme = getItem("theme");
+    if (!!prevTheme && prevTheme.length > 0) {
+      return prevTheme;
+    } else {
+      const systemTheme = getSystemTheme();
+      return systemTheme;
+    }
+  });
+
+  // function to toggle theme
+  function toggleTheme() {
+    if (theme === THEME.DARK) {
+      setTheme(THEME.LIGHT);
+      setItem("theme", THEME.LIGHT);
+    } else {
+      setTheme(THEME.DARK);
+      setItem("theme", THEME.DARK);
+    }
+  }
+
+  useEffect(() => {
+    if (theme === THEME.DARK) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  return { theme, toggleTheme };
+}
+
+// HELPER FUNCTIONS
+
+/**
+ * function to get the system theme
+ * @returns string
+ */
+const getSystemTheme = () => {
+  if (typeof window !== "undefined") {
+    const isDark = window.matchMedia(MEDIA).matches;
+    if (isDark) return THEME.DARK;
+  }
+  return THEME.LIGHT;
+};
